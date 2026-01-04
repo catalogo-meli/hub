83879f3d0f8a6a167b2bcbf91f7d8b6b895200d5
Proyecto Catálogo <ezequiel.galarza@grupokelsoft.com>
2026-01-04 12:37:24 -0300

    Create config.js

---
+++ config.js
@@ -0,0 +1,4 @@
+window.APP_CONFIG = {
+  GAS_URL: "https://script.google.com/macros/s/AKfycbzd_ZJYZasbgMA729bXNFc5N444QzGXJoGl7PaGqddH5ohL01thvtVYrh4mTJMi3pDXKQ/exec",
+  API_TOKEN: "TU_TOKEN_ACA"
+};
