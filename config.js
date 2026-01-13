cfcc34b33753e43c1422867f8b1311dba7841131
Proyecto Catálogo <ezequiel.galarza@grupokelsoft.com>
2026-01-12 21:36:52 -0300

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,8 @@
+// config.js — FIX
 window.APP_CONFIG = {
-  GAS_URL: "https://script.google.com/macros/s/AKfycbzd_ZJYZasbgMA729bXNFc5N444QzGXJoGl7PaGqddH5ohL01thvtVYrh4mTJMi3pDXKQ/exec",
-  API_TOKEN: "hub_2026_catálogo_9f3a7c1d8b2e4a6d"
+  // Usá el proxy de Netlify (recomendado)
+  GAS_URL: "/.netlify/functions/gas",
+
+  // ESTE TIENE QUE SER TU TOKEN REAL (el mismo que probaste en la URL)
+  API_TOKEN: "hub_2026_catálogo_9f3a7c1d8b2e4a6d",
 };
