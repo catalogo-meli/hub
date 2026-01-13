18d4eddbf0eb6fb3fd13b6476f7d9f2ac247b86e
Proyecto Catálogo <ezequiel.galarza@grupokelsoft.com>
2026-01-12 21:23:50 -0300

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 window.APP_CONFIG = {
   GAS_URL: "https://script.google.com/macros/s/AKfycbzd_ZJYZasbgMA729bXNFc5N444QzGXJoGl7PaGqddH5ohL01thvtVYrh4mTJMi3pDXKQ/exec",
-  API_TOKEN: "TU_TOKEN_ACA"
+  API_TOKEN: "hub_2026_catálogo_9f3a7c1d8b2e4a6d"
 };
